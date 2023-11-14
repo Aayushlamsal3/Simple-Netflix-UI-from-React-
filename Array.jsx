45c19e0c4ae0951a2049c720093dc8a909dcc07f
Aayushlamsal3 <132969642+Aayushlamsal3@users.noreply.github.com>
2023-11-14 23:19:07 +0545

    Create Array.jsx

---
+++ Array.jsx
@@ -0,0 +1,63 @@
+const Arr=[
+{    
+imgsrc:"https://images.ctfassets.net/4cd45et68cgf/3BJQrujAXLeXXsWXnolmvB/28845a87ecb25a0609c2e9faec57736c/EN_SQdGame_Main_PlayGround_Horizontal_RGB_PRE.jpg?w=1200",
+title:"A Netflix Original Series",
+btn:"Watch Now"
+},
+{    
+imgsrc:"https://m.media-amazon.com/images/M/MV5BZTY5ZmMwMDQtMmYxYy00ZTYyLWE5MDItZGUwM2I5NTAwOGUwXkEyXkFqcGdeQXVyMTkxNjUyNQ@@._V1_FMjpg_UX1000_.jpg",
+title:"A Netflix Original Series",
+btn:"Watch Now"
+},
+{    
+imgsrc:"https://imageio.forbes.com/blogs-images/markhughes/files/2015/02/50-Shades-of-Grey-2.png?format=png&height=900&width=1600&fit=bounds",
+title:"A Netflix Original Series",
+btn:"Watch Now"
+},
+{    
+imgsrc:"https://assets-prd.ignimgs.com/2021/09/15/moneyheist-1631728690722.png",
+title:"A Netflix Original Series",
+btn:"Watch Now"
+},
+{    
+imgsrc:"https://m.media-amazon.com/images/M/MV5BMTdkMjUyOWQtZTgzZC00OWUyLTgxNTctMzFiZWI5NzE4OGM3XkEyXkFqcGdeQXVyMTI4MTk2NzMz._V1_.jpg",
+title:"A Netflix Original Series",
+btn:"Watch Now"
+},
+{    
+imgsrc:"https://m.media-amazon.com/images/M/MV5BY2NlYjZiYjItNTljNy00MzU2LWJkYmMtMjNkMWVjYmYwMzExXkEyXkFqcGdeQXVyMTEyMjM2NDc2._V1_FMjpg_UX1000_.jpg",
+title:"A Netflix Original Series",
+btn:"Watch Now"
+},
+{    
+imgsrc:"https://m.media-amazon.com/images/M/MV5BZjYzZDgzMmYtYjY5Zi00YTk1LThhMDYtNjFlNzM4MTZhYzgyXkEyXkFqcGdeQXVyMTE5NDQ1MzQ3._V1_.jpg",
+title:"A Netflix Original Series",
+btn:"Watch Now"
+},
+{    
+imgsrc:"https://www.koimoi.com/wp-content/new-galleries/2021/10/little-things-season-4-review-out-001.jpg",
+title:"A Netflix Original Series",
+btn:"Watch Now"
+},
+{    
+imgsrc:"https://i.ytimg.com/vi/aG8KWYko8VY/maxresdefault.jpg",
+title:"A Netflix Original Series",
+btn:"Watch Now"
+},
+{    
+imgsrc:"https://m.media-amazon.com/images/M/MV5BYzQwMGZlYTUtODUwNi00ZjQxLWEzODAtZGU3Zjc0MmNhMzhkXkEyXkFqcGdeQXVyNTkzNDQ4ODc@._V1_FMjpg_UX1000_.jpg",
+title:"A Netflix Original Series",
+btn:"Watch Now"
+},
+{    
+imgsrc:"https://m.media-amazon.com/images/M/MV5BNmEzOWVkYjEtMTdjMy00OTljLWIzYzctNTg3ZTQ2Mzc0NTA2XkEyXkFqcGdeQXVyNDAzNDk0MTQ@._V1_.jpg",
+title:"A Netflix Original Series",
+btn:"Watch Now"
+},
+{    
+imgsrc:"https://resizing.flixster.com/0xxuABVVuzJrUT130WFHKE-irEg=/ems.cHJkLWVtcy1hc3NldHMvdHZzZWFzb24vNzUyMTFhOTktZTU4Ni00ODkyLWJlYjQtZTgxYTllZmU2OGM0LmpwZw==",
+title:"A Netflix Original Series",
+btn:"Watch Now"
+}
+]
+export default Arr;
